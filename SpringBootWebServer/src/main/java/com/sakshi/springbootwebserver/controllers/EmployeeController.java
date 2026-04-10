d99257b8ba16c28dfef9da4c9cf5add1584ba576
sakshisonje <sonjesakshi@gmail.com>
2026-04-10 14:44:32 +0530

    @RequestMapping append

--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/controllers/EmployeeController.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/controllers/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.sakshi.springbootwebserver.controllers;
 
 import com.sakshi.springbootwebserver.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path="/employees")
 public class EmployeeController {
 
 //    @GetMapping(path="/getSecretMessage")
@@ -16,11 +14,11 @@ public class EmployeeController {
 //        return "This is the secret message : Getmapping is implemented";
 //    }
 
-    @GetMapping("/employees/{employeeId}")
+    @GetMapping("/{employeeId}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
         return new EmployeeDTO(employeeId,"Sakshi Sonje","sak@gmail.com",24, LocalDate.of(2023,10,7),true);
     }
-    @GetMapping(path="/employees")
+    @GetMapping
     public String getAllEmployees(@RequestParam(required = false) Integer age,
                                   @RequestParam(required = false) String sortBy){
          return "Hi age   "+age+"    "+sortBy;
