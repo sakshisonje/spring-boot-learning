8970129499a0a679e6772eb700fd8de173128cdf
sakshisonje <sonjesakshi@gmail.com>
2026-04-15 20:28:36 +0530

    Modelmapper Config

---
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/configs/MapperConfig.java
@@ -0,0 +1,12 @@
+package com.sakshi.springbootwebserver.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+}
