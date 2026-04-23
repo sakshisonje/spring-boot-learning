a2ace3b4ea065ab9ae587f929e3eddf45e8a290b
Sakshi Sonje <sonjesakshi2002@gmail.com>
2026-04-23 18:41:43 +0530

    Define Custom annotation

--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/annotations/EmployeeRoleValidation.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/annotations/EmployeeRoleValidation.java
@@ -6,7 +6,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+//Define Annotation
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD,ElementType.PARAMETER})
 @Constraint(validatedBy={EmployeeRoleValidator.class})
--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/annotations/EmployeeRoleValidator.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/annotations/EmployeeRoleValidator.java
@@ -6,7 +6,6 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.List;
 
 public class EmployeeRoleValidator implements ConstraintValidator<EmployeeRoleValidation, String> {
-
     @Override
     public boolean isValid(String inputRole, ConstraintValidatorContext constraintValidatorContext) {
         List<String> roles= List.of("USER","ADMIN", "SDE", "QE", "PO", "SM");
