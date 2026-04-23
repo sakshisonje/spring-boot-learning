488af755e17c361d28b4add9f7cd2a1ceec25dbb
Sakshi Sonje <sonjesakshi2002@gmail.com>
2026-04-24 00:46:08 +0530

    Consise error format in generated Response message

--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/advices/GlobalExceptionHandler.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/advices/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.sakshi.springbootwebserver.advices;
 import com.sakshi.springbootwebserver.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,5 +32,19 @@ public class GlobalExceptionHandler {
                 .message(exception.getMessage())
                 .build();
         return new ResponseEntity<>(apiError,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleInputValidationErrors(MethodArgumentNotValidException exception){
+        List<String> errors=exception.getBindingResult().getAllErrors()
+                .stream()
+                .map(error-> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ApiError apiError=ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(errors.toString())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.BAD_REQUEST);
+
     }
 }
