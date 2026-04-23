9388391a555f390af244dbb304e191d58dd42623
sakshisonje <sonjesakshi@gmail.com>
2026-04-23 13:07:48 +0530

    Patch Mapping Reflecton logic

--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/services/EmployeeService.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/services/EmployeeService.java
@@ -5,7 +5,9 @@ import com.sakshi.springbootwebserver.entities.EmployeeEntity;
 import com.sakshi.springbootwebserver.repositeries.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -66,6 +68,15 @@ public class EmployeeService {
         if(!exists) return null;
 
         EmployeeEntity employeeEntity=employeeRepository.findById(employeeId).get();
+        updates.forEach((field,value)->{
+            Field fieldTobeUpdated= ReflectionUtils.findField(EmployeeEntity.class,field);
+//                    .findRequiredField(EmployeeEntity.class,field);
+            fieldTobeUpdated.setAccessible(true); //private fields use kar sakte
+            ReflectionUtils.setField(fieldTobeUpdated, employeeEntity,value);
 
+            //we are going in each and every update inside employeeentity class...
+        });
+
+        return modelMapper.map(employeeRepository.save(employeeEntity), EmployeeDTO.class);
     }
 }
