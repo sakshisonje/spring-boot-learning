4f169e846749b945d72eaa568c4282b66a88c885
Sakshi Sonje <sonjesakshi2002@gmail.com>
2026-05-03 12:45:47 +0530

    Created DTO layer presentation layer
    Signed-off-by: Sakshi Sonje <sonjesakshi2002@gmail.com>

--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/dto/DepartmentDTO.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/dto/DepartmentDTO.java
@@ -1,4 +1,30 @@
 package com.sakshi.springbootwebserver.dto;
 
+import com.sakshi.springbootwebserver.annotations.DepartmentTitleValidation;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DepartmentDTO {
+    @Null(message = "ID should be null while creating")
+    private Long id;
+
+    @NotBlank(message = "Title cannot be blank")
+    @DepartmentTitleValidation
+    private String title;
+
+    @AssertTrue(message = "Department must be active")
+    private Boolean isActive;
+
+    @PastOrPresent
+    private LocalDate createdAt;
 }
