66f74dc89733f8bd9d41d8e5097b078a5cdee927
Sakshi Sonje <sonjesakshi2002@gmail.com>
2026-05-03 20:26:09 +0530

    Declaring Getmapping in controller
    Signed-off-by: Sakshi Sonje <sonjesakshi2002@gmail.com>

--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/controllers/DepartmentController.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/controllers/DepartmentController.java
@@ -1,8 +1,15 @@
 package com.sakshi.springbootwebserver.controllers;
 
+import com.sakshi.springbootwebserver.dto.DepartmentDTO;
 import com.sakshi.springbootwebserver.services.DepartmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path="/departments")
@@ -11,5 +18,20 @@ public class DepartmentController {
     private final DepartmentService departmentService;
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentDTO>> getAllDepartments() {
+
+        return ResponseEntity.ok(
+                departmentService.getAllDepartments()
+        );
+    }
+    @GetMapping(path = "/{departmentId}")
+    public ResponseEntity<DepartmentDTO> getDepartmentById(
+            @PathVariable(name="departmentId") Long id){
+        Optional<DepartmentDTO> departmentDTO= departmentService.getDepartmentById(id);
+
     }
 }
--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/services/DepartmentService.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/services/DepartmentService.java
@@ -1,8 +1,16 @@
 package com.sakshi.springbootwebserver.services;
 
+import com.sakshi.springbootwebserver.dto.DepartmentDTO;
+import com.sakshi.springbootwebserver.dto.EmployeeDTO;
+import com.sakshi.springbootwebserver.entities.DepartmentEntity;
+import com.sakshi.springbootwebserver.entities.EmployeeEntity;
 import com.sakshi.springbootwebserver.repositeries.DepartmentRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DepartmentService {
@@ -13,5 +21,16 @@ public class DepartmentService {
     public DepartmentService(DepartmentRepository departmentRepository, ModelMapper modelMapper) {
         this.departmentRepository = departmentRepository;
         this.modelMapper = modelMapper;
+    }
+
+    public List<DepartmentDTO> getAllDepartments() {
+        List<DepartmentEntity> departmentEntities=departmentRepository.findAll();
+        return departmentEntities
+                .stream()
+                .map(departmentEntity -> modelMapper.map(departmentEntity, DepartmentDTO.class))
+                .collect(Collectors.toList());
+    }
+    public Optional<DepartmentDTO> getDepartmentById(Long id){
+
     }
 }
