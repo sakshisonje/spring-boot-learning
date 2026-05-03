313ed4a6be4bbf66bcee8bc96079e45fc9f201ee
Sakshi Sonje <sonjesakshi2002@gmail.com>
2026-05-03 14:32:11 +0530

    Created DepartmentService service layer. Declared departmentrepository to acccess DB ops. Modemapper maps DTO entity
    Signed-off-by: Sakshi Sonje <sonjesakshi2002@gmail.com>

--- SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/services/DepartmentService.java
+++ SpringBootWebServer/src/main/java/com/sakshi/springbootwebserver/services/DepartmentService.java
@@ -1,4 +1,14 @@
 package com.sakshi.springbootwebserver.services;
 
+import com.sakshi.springbootwebserver.repositeries.DepartmentRepository;
+import com.sakshi.springbootwebserver.repositeries.EmployeeRepository;
+import org.modelmapper.ModelMapper;
+
 public class DepartmentService {
+    private final DepartmentRepository departmentRepository;  //service needs DB operations
+    private final ModelMapper modelMapper;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
 }
